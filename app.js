1c054ba183fc14aca20cdc67764d533074d7d374
Indraneel <neel16112001@gmail.com>
2021-12-17 16:16:04 +0530

    Feat : Level-5 Authentication,cookies and seesions using passport.js Body : Implemented passport.js and its dependencies to generate and intialize a session.        Using authentication methods from passport to store the hash and salts of the respectiver users in userDB.

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 //jshint esversion:6
 //L-2 : Encryption
-require('dotenv').config();
+// require('dotenv').config();
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+// const encrypt = require('mongoose-encryption');
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-//L-3 Hashing
-const sha512 = require('js-sha512');
-//L-4 hashing + Salting
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+// //L-3 Hashing
+// const sha512 = require('js-sha512');
+// //L-4 hashing + Salting
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10;
+
+//L-5 Session
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+
 
 const app = express();
 const port = 3000;
@@ -20,6 +27,17 @@ app.use(express.static("public"));
 
 app.set('view engine','ejs');
 
+//Creating a Session by passing a secret,without resaving and saveUnintialized is false
+app.use(session({
+ secret : "MyLittlesecret",
+ resave : false,
+ saveUninitialized:false
+}));
+
+//Intialize the passport session
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect('mongodb://localhost:27017/userdb');
 
 const userSchema = new mongoose.Schema({
@@ -27,19 +45,18 @@ const userSchema = new mongoose.Schema({
     password : String
 });
 
-
-
-//Using a secret random string to encrypt the password of userDB using AES-256-CBC encryption.
-//.For more info : https://www.npmjs.com/package/mongoose-encryption
-//plugins the encrypt function by using the secret string.
-//encryptedFields option is used to encrypt certain fields only.
-//Save-encrypt find-decrypt
-// userSchema.plugin(encrypt,{secret : process.env.SECRET, encryptedFields: ["password"]});
+//Plugin Passport Local Mongoose
+userSchema.plugin(passportLocalMongoose);
 
 
 const User = new mongoose.model('user',userSchema);
 
-
+//Use the Strategy on the user i.e email and password 
+passport.use(User.createStrategy());
+//Serialize -creates the the cookie
+passport.serializeUser(User.serializeUser());
+//and deserialize - Deletes the cookie
+passport.deserializeUser(User.deserializeUser());
 
 
 app.listen(port , function(request,response)
@@ -65,20 +82,35 @@ app.get("/register",function(request,myServerResponse)
      
 });
 
+app.get("/secrets",function(request,myServerResponse){
+    //Here the user is authenticated i.e user is logged in to the session the secrets page is rendered
+   if(request.isAuthenticated())
+   myServerResponse.render("secrets");
+   else 
+   //Else the user is redirected to login page
+   myServerResponse.redirect("/login");
+
+});
+
+
+
+
+
 app.post("/register",function(request,myServerResponse){
-   //Passing the password into the hash function and salting.
-   //Storing the shash in DB
-    bcrypt.hash(request.body.password,saltRounds,function(err,hash){
-        const newUsr = new User({
-            email : request.body.username,
-            password : hash
-        });
-        newUsr.save(function(err){
-            if(err)
-            console.log(err);
-            else
-            myServerResponse.render("secrets");
-          });
+//Registering the username nad password and authenticating using local strategy by passport-local-mongoose
+    User.register({username: request.body.username},request.body.password,function(err,user){
+               if(err)
+               {
+                   //If any error occurs,user is redirected to register route
+               console.log(err);
+               myServerResponse.redirect("register");
+               }
+               else{
+                   //else the user is authenticated and redirected to /secrets route
+                passport.authenticate("local")(request,myServerResponse,function(){
+                       myServerResponse.redirect("/secrets");
+                });
+               }
     });
    
 
@@ -89,30 +121,28 @@ app.post("/register",function(request,myServerResponse){
 });
 
 app.post("/login",function(request,myServerResponse){
-    const userName = request.body.username;
-    const userPass = request.body.password;
-
-    console.log(userPass);
-   
-
-    User.findOne({email : userName},function(err,foundUser){
-           if(err)
-           console.log(err);
-           //Compare the passwords stored in DB using bcrypt.compare
-         if(foundUser)
-         {
-                 bcrypt.compare(userPass,foundUser.password,function(err,result){
-                            if(result)
-                            myServerResponse.render("secrets");
-                            else 
-                            console.log(err);
-                 });
-         }
-           else
-           myServerResponse.send("OOPs wrong password");
-
+    
+    const user = new User({
+         username : request.body.username,
+         password : request.body.password
     });
-
+    //Requests a login and authenticates it when the user is found
+    request.login(user,function(err){
+          if(err)
+          console.log(err);
+          else{
+                passport.authenticate('local')(request,myServerResponse,function(){
+                     myServerResponse.redirect("/secrets");
+                });
+          }
+    });
+   
       
 });
 
+
+app.get("/logout",function(request,myServerResponse){
+    //Log-out off the session 
+      request.logout();
+      myServerResponse.redirect("/");
+});
